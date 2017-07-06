9e49f32fe9c16f9fc7d3fe9d6224db1fc6d10b68
Pavel Ivonin <pavelivonin@yandex.ru>
2017-07-06 21:56:02 +0200

    add left column

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,8 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/jQuery.mmenu/dist/jquery.mmenu.all.js',
+		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
