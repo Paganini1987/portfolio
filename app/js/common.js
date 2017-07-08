8fbc80bdaff05fc8dfa77f32ff9a39ba95f125cb
Pavel Ivonin <pavelivonin@yandex.ru>
2017-07-08 18:23:14 +0200

    add mmenu

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,21 @@
 $(function() {
 
-	// Custom JS
+	$('#my-menu').mmenu({
+		extensions: ['theme-white', 'effect-menu-slide', 'pagedim-black'],
+		navbar: {
+			title: '<div class="avatar"><a class="phone" href="tel:+7-905-870-92-36"><i class="fa fa-phone"></i></a><a href="/"><img src="img/user.jpg" alt="avatar"></a><a class="mail" href="mailto:pavelivonin@yandex.ru"><i class="fa fa-envelope-o"></a></div>'
+		},
+		offCanvas: {
+			position: 'right'
+		}
+	});
+
+	var api = $('#my-menu').data('mmenu');
+	api.bind('open:finish', function() {
+		$('.hamburger').addClass('is-active');
+	});
+	api.bind('close:finish', function() {
+		$('.hamburger').removeClass('is-active');
+	});
 
 });
