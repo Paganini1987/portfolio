67aa6abdc124868077b5564766b0b9b5d4d1753d
Pavel Ivonin <pavelivonin@yandex.ru>
2017-07-09 17:53:24 +0200

    add works

--- app/js/common.js
+++ app/js/common.js
@@ -27,4 +27,22 @@ $(function() {
 		$('.left_column').height(right);
 	}
 
+	$('.image-link').magnificPopup({
+	 	type:'image',
+	 	gallery:{
+	 		enabled: true
+	 	},
+	 	mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+	  zoom: {
+	    enabled: true, // By default it's false, so don't forget to enable it
+
+	    duration: 300, // duration of the effect, in milliseconds
+	    easing: 'ease-in-out', // CSS transition easing function
+
+	    opener: function(openerElement) {
+	      return openerElement.is('div') ? openerElement : openerElement.find('div');
+	    }
+	  }
+	});
 });
